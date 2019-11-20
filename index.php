1de8c6adba90c5bb4453bc3b41c13d551b947c12
isaak-sielleur <55872420+isaak-sielleur@users.noreply.github.com>
2019-11-20 13:00:19 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+      <link rel="stylesheet" type="text/css" href="module.css">
+      <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+      <title>Présentation</title>
+      <link rel="icon" type="png" href="images/logo.png">
+  
+  </head>
+
+  <header class="headaccueil">
+	 <img class="img1accueil" src="images/head.png">
+   <h1 class="h1accueil">Qui sommes nous ?</h1>
+   <p class="paccueil">Nous sommes un Forum,</p>
+   <p class="paccueil">Nous voulons vous offrir une plateforme de conversation simple pour parler de programmation, ici vous pourrez echanger vos codes, poser des questions, le tout par sections bien délimitées. Nous moderons activement le site et nous avons minimum un moderateur connecté jour et nuit, ainsi en cas de soucis la réponse sera toujours immédiate.</p>
+   <p class="paccueil">Inscrivez vous, vous ne serez pas déçus (du moins nous faisont tout notre possible pour que cela ne soit pas le cas). Si vous êtes déja membre de notre communauté nous sommes heureux de savoir que vous revenez.</p>
+   <p class="paccueil">Bonne navigationa tous et toutes, merci de votre confiance.</p>
+  </header>
+
+  <body class="backaccueil">
+
+  <main>
+
+
+  </main>
+
+  <section>
+    <label class="fab">
+
+        <input type="checkbox">
+
+        <div class="fab-menu">
+
+          <div class="fab-btn">+</div>
+
+          <ul>
+            <li tooltip="Se connecter"><a href="connexion.php"><button>Connexion</button></a></li>
+            <li tooltip="S'inscrire"><a href="inscription.php" target="_blank"><button>Inscription</button></a></li>
+          </ul>
+        </div>
+
+        <div class="fab-blank"></div>
+
+    </label>
+
+  </section>
+  </body>
+
+  </html>
---
+++ inscription.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+      <link rel="stylesheet" type="text/css" href="module.css">
+      <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+      <title>Présentation</title>
+      <link rel="icon" type="png" href="images/logo.png">
+      <meta charset="UTF-8">
+  
+  </head>
+
+  <header class="headaccueil">
+
+  </header>
+
+  <body class="backaccueil">
+
+  <main>
+
+
+  </main>
+
+  <section>
+        <div class="body-content">
+
+            <div class="module">
+
+                <h1 class="h1inscri">Créer un compte</h1>
+
+                <form class="form" action="" method="post" enctype="multipart/form-data" autocomplete="off">;
+
+                    <div class="alert alert-error"></div>
+                    <input class="inputinscri" type="text" placeholder="Utilisateur" name="login" required />
+
+                    <input class="inputinscri" type="text" placeholder="Prenom" name="prenom" required />
+
+                    <input class="inputinscri" type="text" placeholder="Nom" name="name" required />
+
+                    <input class="inputinscri" type="password" placeholder="Mot de passe" name="password" autocomplete="new-password" required />
+
+                    <input class="inputinscri" type="password" placeholder="Confirmer le mot de passe" name="confirmpassword" autocomplete="new-password" required />
+
+                    <input class="inputinscri" type="submit" value="Register" name="register" class="btn btn-block btn-primary"/>
+
+                </form>
+                <?php
+
+                    if (isset($_POST['register'])) {
+
+                        $login= $_POST ['login'];
+                        $prenom= $_POST ['prenom'];
+                        $nom= $_POST ['name'];
+                        $password= $_POST ['password'];
+                        
+                        $link= mysqli_connect("127.0.0.1", "root", "", "moduleconnexion");
+                        $query= mysqli_query($link, "INSERT INTO utilisateurs (login, prenom, nom, password) VALUES ('$login', '$prenom', '$nom', '$password')");
+
+                        header ('location: connexion.php');
+                        
+                    }
+    
+            ?>
+            </div>
+            </div>
+
+  </section>
+  </body>
+
+  </html>
